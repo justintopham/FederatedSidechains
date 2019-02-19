a7ca817a52cf06e66476e4690ebc359039f3ae1e
Justin Topham <jtopham@Justins-MacBook-Air.local>
2019-02-19 18:10:56 +0000

    Change to the scripts and added federationKey.dat file.

--- LSC_Node_Scripts/start_miner1.sh
+++ LSC_Node_Scripts/start_miner1.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 Message="Changing dir..."
-NodesDirectory="/Users/jtopham/LocalSCNodes"
+NodesDirectory="$HOME/LocalSCNodes"
 echo $Message
 cd ..
 git checkout Test
 cd src/Stratis.LocalSmartContracts.FederationGatewayD
 echo "Running miner 1..."
 echo "**Data held in $NodesDirectory/miner1**"
-dotnet run --no-build -sidechain -datadir="$NodesDirectory/miner1" -port=36201 -apiport=38201 -publickey=02f5b2a2fc2aa9f2ab85e9727720f9b280ed937f897e444810abaada26738b13c4 -mincoinmaturity=1 -mindepositconfirmations=1 -txindex=1 -redeemscript="1 02f5b2a2fc2aa9f2ab85e9727720f9b280ed937f897e444810abaada26738b13c4 1 OP_CHECKMULTISIG" -connect=0 -bind=127.0.0.1
+dotnet run --no-build -sidechain -datadir="$NodesDirectory/miner1" -port=36201 -apiport=38201 -publickey=02f5b2a2fc2aa9f2ab85e9727720f9b280ed937f897e444810abaada26738b13c4 -mincoinmaturity=1 -mindepositconfirmations=1 -txindex=1 -redeemscript="1 02f5b2a2fc2aa9f2ab85e9727720f9b280ed937f897e444810abaada26738b13c4 1 OP_CHECKMULTISIG" -connect=0 -bind=127.0.0.1r 
--- LSC_Node_Scripts/start_node1.sh
+++ LSC_Node_Scripts/start_node1.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 Message="Changing dir..."
-NodesDirectory="/Users/jtopham/LocalSCNodes"
+NodesDirectory="$HOME/LocalSCNodes"
 echo $Message
 cd ..
 git checkout Test
 cd src/Stratis.LocalSmartContracts.StandardSidechainD
 echo "Running standard node 1..."
 echo "**Data held in $NodesDirectory/node1**"
-dotnet run --no-build -sidechain -datadir="$NodesDirectory/node1" -port=36202 -apiport=38202 -connect=127.0.0.1:36201 -bind=127.0.0.1
+dotnet run --no-build -datadir="$NodesDirectory/node1" -port=36202 -apiport=38202 -connect=127.0.0.1:36201 -bind=127.0.0.1
